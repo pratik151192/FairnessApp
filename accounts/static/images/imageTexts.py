3970c919bf154e06cdf2ffd1db4273e2a4cc2379
Pratik <pratikagarwal1@outlook.com>
2017-12-01 16:04:59 -0500

    Fixed Images

--- accounts/static/images/imageTexts.py
+++ accounts/static/images/imageTexts.py
@@ -9,21 +9,6 @@ texts = {
     "8": "IntelliJ's project structure now allows to set root sources",
     "9": "This cute little dog's image has been posted by a criminal who claims it to be his next victim.",
     "10": "Narendra Modi claims doing business in India is easier than before.",
-    "11": "USCIS claims that they have added case histories to make students' lives 'easier'.",
-    "12": "IntelliJ's project structure now allows to set root sources",
-    "13": "This cute little dog's image has been posted by a criminal who claims it to be his next victim.",
-    "14": "Narendra Modi claims doing business in India is easier than before.",
-    "15": "USCIS claims that they have added case histories to make students' lives 'easier'.",
-    "16": "IntelliJ's project structure now allows to set root sources",
-    "17": "This cute little dog's image has been posted by a criminal who claims it to be his next victim.",
-    "18": "Narendra Modi claims doing business in India is easier than before.",
-    "19": "USCIS claims that they have added case histories to make students' lives 'easier'.",
-    "20": "IntelliJ's project structure now allows to set root sources",
-    "21": "This cute little dog's image has been posted by a criminal who claims it to be his next victim.",
-    "22": "Narendra Modi claims doing business in India is easier than before.",
-    "23": "USCIS claims that they have added case histories to make students' lives 'easier'.",
-    "24": "IntelliJ's project structure now allows to set root sources",
-    "25": "IntelliJ's project structure now allows to set root sources",
 }
 names = ['Monica', 'Chandler', 'Joey', 'Phoebe', 'Rachel', 'Ross', 'Walter', 'Jesse', 'Kristen', 'Kathy', 'Monica', 'Chandler', 'Joey', 'Phoebe', 'Rachel', 'Ross', 'Walter', 'Jesse', 'Kristen', 'Kathy']
 settings = {
