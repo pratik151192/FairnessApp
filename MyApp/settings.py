72a8863e9e81b46d27de50c1c56f6450a0d17f83
Pratik <pratikagarwal1@outlook.com>
2017-12-02 13:41:07 -0500

    Fixed static

--- MyApp/settings.py
+++ MyApp/settings.py
@@ -130,7 +130,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, 'accounts/static/images'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
