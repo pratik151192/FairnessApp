ca9d43d71131af3c45e4d029f9363c4a5c25fb29
Pratik <pratikagarwal1@outlook.com>
2017-12-02 13:24:19 -0500

    Fixed static

--- MyApp/settings.py
+++ MyApp/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticaa')
 print(STATIC_ROOT)
 STATIC_URL = '/static/'
 
