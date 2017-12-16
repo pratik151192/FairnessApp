c1b354b562927d7cf92dcb9b15fd19619284508f
Pratik <pratikagarwal1@outlook.com>
2017-12-16 12:58:58 -0500

    Added models to admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import UserProfile
+from .models import UserProfile, UserValues, Robots
 # Register your models here.
 admin.site.register(UserProfile)
+admin.site.register(UserValues)
+admin.site.register(Robots)
